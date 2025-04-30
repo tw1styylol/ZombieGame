36ffa12f346e2817d32dfbc011b185b7a7b5cf76
Костянтин Борисюк <KOSTIANTYN.BORYSIUK@lnu.edu.ua>
2025-04-30 20:58:44 +0300

    weapons cs

--- weapon.cs/weapon.cs
+++ weapon.cs/weapon.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Weapon
+{
+    public string Name { get; protected set; }
+    public int DamageReduction { get; protected set; }
+
+    public bool TryBreak()
+    {
+        Random rand = new Random();
+        return rand.Next(100) < 50;
+    }
+}
+
+class MeleeWeapon : Weapon
+{
+    public MeleeWeapon()
+    {
+        Name = "Ніж";
+        DamageReduction = 5;
+    }
+}
+
+class Pistol : Weapon
+{
+    public Pistol()
+    {
+        Nama = "Пістолет";
+        DamageReduction = 10;
+    }
+}
+
+class Rifle : Weapon
+{
+    public Rifle()
+    {
+        Name = "Гвинтівка";
+        DamageReduction = 25;
+    }
+}
+
+class Shotgun : Weapon
+{
+    public Shotgun()
+    {
+        Name = "Дробовик";
+        DamageReduction = 35;
+    }
+}
+
+
+class FlareGun : Weapon
+{
+    public FlareGun()
+    {
+        Name = "Сигнальний пістолет";
+        DamageReduction = 0;
+    }
+}
